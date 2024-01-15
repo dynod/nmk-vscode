4abecdbede364d1a3371248a4a22ab12a7b0dbe5
David Pochet <david.pochet@gmail.com>
2024-01-15 07:58:27 +0000

    Use buildenv rather than loadme in VSCode tasks

--- doc/conf.py
+++ doc/conf.py
@@ -7,7 +7,7 @@
 
 # Project information
 project = "nmk-vscode"
-copyright = "2023, The dynod project"
+copyright = "2024, The dynod project"
 author = "The dynod project"
 
 # Extensions configuration
